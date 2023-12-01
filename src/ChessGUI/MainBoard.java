08985bce4f6c1f316d9f6019d30d3bf0c07400bd
Saif eldeen Adel <saifadel77@hotmail.com>
2023-12-01 09:50:30 +0200

    Added some comments

--- src/ChessGUI/MainBoard.java
+++ src/ChessGUI/MainBoard.java
@@ -11,17 +11,20 @@ import java.awt.event.MouseEvent;
 public class MainBoard extends JFrame {
     private ChessGame game;
     private JPanel boardPanel;
-    private JPanel prevSquare = null;
+    private JPanel prevClickedSquare = null;
 
     public MainBoard() {
         game = new ChessGame();
 
+        // Creating the main panel of the whole board and setting an 8x8 grid inside it
         boardPanel = new JPanel();
         boardPanel.setLayout(new GridLayout(8, 8));
 
+        // Filling in the board panel then adding it to our frame
         initialiseSquares();
         add(boardPanel, BorderLayout.CENTER);
 
+        // Setting the dimensions of our main window.
         this.setTitle("8139 & 8277's Chess");
         this.setSize(1000,1000);
         this.setLocation(450,20);
@@ -31,26 +34,32 @@ public class MainBoard extends JFrame {
 
     public void initialiseSquares(){
         int count = 0;
+        // Goes through the 8x8 grid and add JPanels which are our "squares" with alternating colors
         for (int rank = 0; rank < Constants.BOARD_HEIGHT; rank++) {
             for (int file = 0; file < Constants.BOARD_WIDTH; file++) {
                 final int rankFinal = rank;
                 final int fileFinal = file;
                 JPanel square = new JPanel();
+                //TODO
+                // Probably fill the squares with the appropriate image by checking the PieceType in game.getBoard().getSquare(rank,file).getPiece().getType()
+                // And choosing the image based on the PieceType and color?
 
+                // Adding the clicking event mouse listener
                 square.addMouseListener(new MouseAdapter() {
                     @Override
                     public void mouseClicked(MouseEvent e) {
                         super.mouseClicked(e);
-                        if (prevSquare != null) {
+                        if (prevClickedSquare != null) {
                             // Reset color of the previously clicked square
-                            Color prevColor = prevSquare.getBackground();
-                            prevSquare.setBackground((prevColor == TileColors.LIGHT_ACCENT) ? TileColors.LIGHT : TileColors.DARK);
+                            Color prevColor = prevClickedSquare.getBackground();
+                            prevClickedSquare.setBackground((prevColor == TileColors.LIGHT_ACCENT) ? TileColors.LIGHT : TileColors.DARK);
                         }
-                        prevSquare = square;
+                        prevClickedSquare = square;
 
                         Color currentColor = square.getBackground();
                         square.setBackground((currentColor == TileColors.LIGHT) ? TileColors.LIGHT_ACCENT: TileColors.DARK_ACCENT);
                         System.out.println("Clicked on File: " + fileFinal + ", Rank: " + rankFinal);
+//                        System.out.println(game.getBoard().getSquare(rankFinal, fileFinal).getPiece().getType());
 
                     }
                 });
